cc67552d5fbc44371e2337793588df2c6234e6fb
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-18 11:36:11 -0300

    Criado controller para Product.

---
+++ DemoRepositoryPattern/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using DemoRepositoryPattern.Infraestructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoRepositoryPattern.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var products = _context.Products.AsNoTracking().ToList();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
