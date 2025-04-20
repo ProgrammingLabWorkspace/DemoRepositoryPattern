699ce47962a0b6507b82967ddce8f8ee71f2a72a
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-20 09:30:33 -0300

    Criado context.

---
+++ LabStore/LabStore.Infraestructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LabStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace LabStore.Infraestructure.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(InfraExtension).Assembly);
+        }
+    }
+}
