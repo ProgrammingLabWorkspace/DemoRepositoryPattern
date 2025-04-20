003aa6eedf96f8cb06924cb5ffef57aac3bf0f9d
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-20 09:29:10 -0300

    Mapeando entidade Product.

---
+++ LabStore/LabStore.Infraestructure/Data/Mapping/ProductMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using LabStore.Domain.Entities;
+
+namespace LabStore.Infraestructure.Data.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired(true)
+                .HasMaxLength(160)
+                .HasColumnType("VARCHAR");
+        }
+    }
+}
