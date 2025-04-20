d4d061b186cca7af70d1025123571ca35a80ea78
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-20 09:30:26 -0300

    Configuração da camada Application.

---
+++ LabStore/LabStore.Application/ApplicationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LabStore.Application
+{
+    public static class ApplicationExtension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddMediatR(med =>
+            {
+                med.RegisterServicesFromAssembly(typeof(ApplicationExtension).Assembly);
+            });
+
+            return services;
+        }
+    }
+}
