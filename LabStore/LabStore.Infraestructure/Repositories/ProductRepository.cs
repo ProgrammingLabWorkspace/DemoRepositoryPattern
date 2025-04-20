e6496a749d550ec95e35c60d0a939de1f83e6dc4
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-20 09:30:57 -0300

    Criado repositório para produto.

---
+++ LabStore/LabStore.Infraestructure/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using LabStore.Domain.Entities;
+using LabStore.Domain.Repositories;
+using LabStore.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabStore.Infraestructure.Repositories
+{
+    public class ProductRepository(AppDbContext context) : IProductRepository
+    {
+        public async Task<Product> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+
+            return product;
+        }
+    }
+}
