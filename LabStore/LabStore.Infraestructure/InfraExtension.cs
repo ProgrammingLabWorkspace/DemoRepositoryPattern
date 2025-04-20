36eb959959a5d184b87fd0cd34877a0ffece5af5
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-04-20 09:30:45 -0300

    Configurado camada infra.

---
+++ LabStore/LabStore.Infraestructure/InfraExtension.cs
@@ -0,0 +1,15 @@
+﻿using LabStore.Domain.Repositories;
+using LabStore.Infraestructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LabStore.Infraestructure
+{
+    public static class InfraExtension
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services) {
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
